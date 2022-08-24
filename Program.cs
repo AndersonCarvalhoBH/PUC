d7bea5619a687dc95710462bdb43e0563289b8e1
Anderson Carvalho <andersondesigner@gmail.com>
2022-08-24 20:06:43 -0300

    Exercicio Par ou Impar

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+            numero =  int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Numero");
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+        }
+    }
+}
