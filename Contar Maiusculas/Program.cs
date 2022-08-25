c1d47d12f27e1ed0f8f891b35c881f721db8611b
Anderson Carvalho <andersondesigner@gmail.com>
2022-08-24 21:07:14 -0300

    ENTREGA 24-08-22

---
+++ Contar Maiusculas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int contalinhas = 0;
+            int[] Vector = new int[100000];
+            do
+            {
+                int contamais = 0;
+                str = Console.ReadLine();
+
+                if (! str.ToLower().Equals("fim") == false) continue;
+
+                {
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        char ch = str[i];
+                        if (ch >= 'A' && ch <= 'Z')
+                        {
+                            contamais++;
+                        }
+                    }
+                    Vector[contalinhas] = contamais;
+                    contalinhas++;
+                }
+                
+            } while (str.ToLower().Equals("fim") == false);
+            for (int i = 0; i < contalinhas; i++)
+            {
+                Console.WriteLine(Vector[i]);
+            }
+        }
+    }
+}
